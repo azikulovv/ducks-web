09b007e790f4cc542b22c51aa1e2feb172e56b36
Maulen <azikulovv@gmail.com>
2026-05-06 13:25:47 +0500

    feat: обновлена логика получения данных пользователя и улучшена обработка отображения

--- app/pages/profile.vue
+++ app/pages/profile.vue
@@ -3,52 +3,38 @@ definePageMeta({
   middleware: 'auth',
 })
 
-const auth = useAuth()
+const { user } = useAuth()
 
-const user = {
-  name: 'Alexey Duck',
-  id: 'DUCKS-24-001',
-  stats: [
-    { label: 'Покер', rank: 12 },
-    { label: 'Дартс', rank: 8 },
-    { label: 'Бильярд', rank: 15 },
-  ],
-}
 const menu = [
   { label: 'Идеи и предложения', path: '/ideas' },
   { label: 'Достижения', path: '#' },
   { label: 'Настройки профиля', path: '#' },
 ]
-
-onMounted(async () => {
-  await auth.fetchMe()
-  console.log(auth.user.value)
-})
 </script>
 
 <template>
-  <div class="p-6 space-y-8">
+  <div class="min-h-screen p-6 space-y-8">
     <div class="bg-[#1A1A1A] rounded-4xl p-8 text-center border border-white/5">
       <div
         class="w-24 h-24 bg-[#0F0F0F] rounded-full mx-auto border-2 border-[#E11D48] flex items-center justify-center text-5xl"
       >
         🕶️
       </div>
-      <h2 class="text-2xl font-black mt-4 italic uppercase">{{ user.name }}</h2>
+      <h2 class="text-2xl font-black mt-4 italic uppercase">{{ user?.name }}</h2>
       <p class="text-[#E11D48] text-[10px] font-bold uppercase tracking-widest">
-        ID: {{ user.id }}
+        ID: {{ user?.id }}
       </p>
     </div>
 
     <div class="grid grid-cols-3 gap-3">
-      <div
-        v-for="s in user.stats"
+      <!-- <div
+        v-for="s in user?.ratings"
         :key="s.label"
         class="bg-[#1A1A1A] p-4 rounded-2xl text-center border border-white/5"
       >
         <p class="text-[9px] text-gray-500 font-black uppercase italic">{{ s.label }}</p>
         <p class="text-xl font-black text-[#E11D48]">#{{ s.rank }}</p>
-      </div>
+      </div> -->
     </div>
 
     <div class="space-y-3">
